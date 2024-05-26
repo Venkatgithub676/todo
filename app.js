550b975fe4652804efbdd71ace4ebb7549bd2e11
venkat <venkat>
2024-05-26 19:05:34 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const sqlite = require("sqlite");
+const { open } = sqlite;
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const dbPath = path.join(__dirname, "todoApplication.db");
+let db = null;
+app.use(express.json());
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server is running at http://localhost:3000/");
+    });
+  } catch (err) {
+    console.log(`DB error : ${err.message}`);
+  }
+};
+
+initializeDBAndServer();
+module.exports = app;
